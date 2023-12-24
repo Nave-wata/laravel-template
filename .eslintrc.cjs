52d0be5e48f1d44a8dc7fafc4fef8d1c4d771f15
Nave3 <90523045+Nave-wata@users.noreply.github.com>
2023-12-24 21:25:20 +0900

    chore: typescriptでセミコロンの強制

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,6 +35,12 @@ module.exports = {
         "react"
     ],
     "rules": {
+        "semi": ["error", "always"],
+        "semi-spacing": ["error", {"after": true, "before": false}],
+        "semi-style": ["error", "last"],
+        "no-extra-semi": "error",
+        "no-unexpected-multiline": "error",
+        "no-unreachable": "error"
     },
     "root": true,
 }
