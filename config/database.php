ce92f3a56fba4b99c8d15c9a9f02b6f4218fc85f
Nave3 <90523045+Nave-wata@users.noreply.github.com>
2023-12-30 01:23:32 +0900

    chore: redisの接続に unix を利用できるように設定の項目を追加（キャッシュ）

--- config/database.php
+++ config/database.php
@@ -147,6 +147,9 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
+
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
+            'path' => env('REDIS_PATH'),
         ],
 
     ],
